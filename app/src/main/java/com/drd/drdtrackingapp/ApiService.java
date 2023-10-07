28505cbaf560d19fcdfcbdfeba1642f98c8e2fc8
kapil707 <kapil707sharma@gmail.com>
2023-10-07 09:30:37 +0530

    add

--- app/src/main/java/com/drd/drdtrackingapp/ApiService.java
+++ app/src/main/java/com/drd/drdtrackingapp/ApiService.java
@@ -35,6 +35,10 @@ public interface ApiService {
     Call<ResponseBody> get_delivery_api(@Field("api_key") String api_key,@Field("user_altercode") String user_altercode);
 
     @FormUrlEncoded
+    @POST("drd_master_api/api01/get_delivery_done_api")
+    Call<ResponseBody> get_delivery_done_api(@Field("api_key") String api_key,@Field("user_altercode") String user_altercode);
+
+    @FormUrlEncoded
     @POST("drd_master_api/api01/get_delivery_chemist_photo_api")
     Call<ResponseBody> get_delivery_chemist_photo_api(@Field("api_key") String api_key,@Field("user_altercode") String user_altercode,@Field("chemist_id") String chemist_id,@Field("gstvno") String gstvno);
 
--- app/src/main/java/com/drd/drdtrackingapp/Home_page.java
+++ app/src/main/java/com/drd/drdtrackingapp/Home_page.java
@@ -58,7 +58,7 @@ public class Home_page extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_delivery_list, R.id.nav_gallery)
+                R.id.nav_home, R.id.nav_delivery_list, R.id.nav_delivery_done_list)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home_page);
