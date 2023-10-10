d5cd1ca2cf8e91a381716db335b67e6dfaa0b24c
kapil707 <kapil707sharma@gmail.com>
2023-10-10 09:54:51 +0530

    add

---
+++ app/src/main/java/com/drd/drdtrackingapp/Qr_code_scan.java
@@ -0,0 +1,14 @@
+package com.drd.drdtrackingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Qr_code_scan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_code_scan);
+    }
+}
