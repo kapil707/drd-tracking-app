ce819390773faa38c84c949ac88b2535e832ae46
kapil707 <kapil707sharma@gmail.com>
2023-10-12 11:33:15 +0530

    add

--- app/src/main/java/com/drd/drdtrackingapp/ApiService.java
+++ app/src/main/java/com/drd/drdtrackingapp/ApiService.java
@@ -10,10 +10,10 @@ import retrofit2.http.POST;
 
 public interface ApiService {
 
-    //@FormUrlEncoded
-    /*@POST("web_api/api01/medicines_last_order_api")
-    Call<ResponseBody> getUserInformation(@Field("user_type") String user_type,@Field("user_altercode") String user_altercode,@Field("user_password") String user_password);
-*/
+    @FormUrlEncoded
+    @POST("drd_master_api/api01/test")
+    Call<ResponseBody> testing(@Field("test") String test);
+
     @FormUrlEncoded
     @POST("drd_master_api/api01/update_tracking_api")
     Call<ResponseBody> update_tracking_api(@Field("api_key") String api_key,@Field("user_code") String user_code,@Field("user_altercode") String user_altercode,@Field("firebase_token") String firebase_token,@Field("getLatitude") String getLatitude,@Field("getLongitude") String getLongitude,@Field("getdate") String getdate,@Field("gettime") String gettime);
@@ -42,14 +42,10 @@ public interface ApiService {
     @POST("drd_master_api/api01/get_delivery_order_photo_api")
     Call<ResponseBody> get_delivery_order_photo_api(@Field("api_key") String api_key,@Field("user_altercode") String user_altercode,@Field("chemist_id") String chemist_id,@Field("gstvno") String gstvno);
 
-
     @FormUrlEncoded
     @POST("drd_master_api/api01/upload_delivery_order_completed_api")
     Call<ResponseBody> upload_delivery_order_completed_api(@Field("api_key") String api_key,@Field("user_altercode") String user_altercode,@Field("chemist_id") String chemist_id,@Field("gstvno") String gstvno,@Field("message") String message,@Field("getLatitude") String getLatitude,@Field("getLongitude") String getLongitude);
 
-    @FormUrlEncoded
-    @POST("drd_master_api/api01/test")
-    Call<ResponseBody> testing(@Field("test") String test);
 
     //Call<ResponseBody> postData(@Body RequestBody requestBody);
 }
