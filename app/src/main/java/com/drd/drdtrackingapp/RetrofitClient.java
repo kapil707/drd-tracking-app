b56de8b5369776dc8d9487cfa527ac08bbfc8714
Kapil <kapil@Kapils-MacBook-Air.local>
2023-11-26 16:00:00 +0530

    add

--- app/src/main/java/com/drd/drdtrackingapp/RetrofitClient.java
+++ app/src/main/java/com/drd/drdtrackingapp/RetrofitClient.java
@@ -4,6 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
+
     public static Retrofit retrofit;
 
     public static Retrofit getRetrofitInstance(){
